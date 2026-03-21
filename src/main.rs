14ec8c6217fd08912f6608adfd11d26433521e63
Elektro <elektro860@github.com>
2026-03-21 21:56:41 +0100

    implemented file picker

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{error::Error, path::Path, sync::Mutex};
+use std::{
+    env,
+    error::Error,
+    ops::Deref,
+    path::{Path, PathBuf},
+    sync::Mutex,
+};
 
-use slint::ComponentHandle;
+use rfd::FileDialog;
+use slint::{ComponentHandle, ModelRc, VecModel};
 
 slint::include_modules!();
 
-static SELECTED_FILE: Mutex<Option<Box<Path>>> = Mutex::new(None);
+static SELECTED_FILE: Mutex<Option<PathBuf>> = Mutex::new(None);
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
-    ui.on_select_file(|| {
-        println!("Selecting file");
+    let ui_weak = ui.as_weak().unwrap();
+    ui.on_select_file(move || {
+        let handle = ui_weak.window().window_handle();
+        let file = FileDialog::new()
+            .set_parent(&handle)
+            .set_directory(env::current_dir().unwrap())
+            .add_filter("JSON", &["json"])
+            .set_title("Select JSON file")
+            .pick_file();
+        if let Some(file) = file {
+            println!("Selected: {:?}", file);
+            let mut m = SELECTED_FILE.lock().unwrap();
+            *m = Some(file);
+        }
     });
 
     let ui_weak = ui.as_weak().unwrap();
